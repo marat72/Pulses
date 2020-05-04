d824faaeeda7fa2067d08f2db096ab5494d6d92a
marat72 <64441254+marat72@users.noreply.github.com>
2020-05-04 22:35:56 +0200

    adaptation

--- js/script.js
+++ js/script.js
@@ -68,8 +68,15 @@ $(document).ready(function(){
 
           $('form').trigger('reset');
       });
+      
       return false;
   });
+          //плавная прокрутка
+  $("a[href^='#']").click(function(){
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
 });
 
 
